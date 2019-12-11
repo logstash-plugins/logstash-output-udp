d9a1873d80ff336f5f5582d0c4bc486a6f90c700
Rob Bavey <rob.bavey@elastic.co>
2019-12-11 14:55:44 -0500

    Don't crash on error when send raises (#10)
    * Don't crash on error when send raises
    
    In certain cases (for example, when a UDP payload is too large),
    the send method can raise an exception, causing the plugin to crash.
    
    This commit will drop the event and log if this occurs.
    
    * Add event payload logging when event is dropped and debug logging is enabled
    
    Will output a truncated event payload if debug logging is enabled

--- lib/logstash/outputs/udp.rb
+++ lib/logstash/outputs/udp.rb
@@ -21,12 +21,32 @@ class LogStash::Outputs::UDP < LogStash::Outputs::Base
   def register
     @socket = UDPSocket.new
     @codec.on_event do |event, payload|
-      @socket.send(payload, 0, @host, @port)
+      begin
+        @socket.send(payload, 0, @host, @port)
+      rescue Errno::EMSGSIZE => e
+        logger.error("Failed to send event, message size of #{payload.size} too long", error_hash(e, payload))
+      rescue => e
+        logger.error("Failed to send event:", error_hash(e, payload))
+      end
+    end
+  end
+
+  MAX_DEBUG_PAYLOAD = 1000
+
+  def error_hash(error, payload)
+    error_hash = {:error => error.inspect,
+                  :backtrace => error.backtrace.first(10)
+                 }
+    if logger.debug?
+      error_hash.merge(:event_payload =>
+                       payload.length > MAX_DEBUG_PAYLOAD ? "#{payload[0...MAX_DEBUG_PAYLOAD]}...<TRUNCATED>" : payload
+                      )
+    else
+      error_hash
     end
   end
 
   def receive(event)
-    
     return if event == LogStash::SHUTDOWN
     @codec.encode(event)
   end
--- spec/outputs/udp_spec.rb
+++ spec/outputs/udp_spec.rb
@@ -3,18 +3,17 @@ require_relative "../spec_helper"
 
 describe LogStash::Outputs::UDP do
 
+  subject { described_class.new(config) }
   let(:host) { "localhost" }
   let(:port) { rand(1024..65535) }
+  let(:config) {{ "host" => host, "port" => port}}
 
   it "should register without errors" do
-    plugin = LogStash::Plugin.lookup("output", "udp").new({ "host" => host, "port" => port})
+    plugin = LogStash::Plugin.lookup("output", "udp").new(config)
     expect { plugin.register }.to_not raise_error
   end
 
   describe "#send" do
-
-    subject { LogStash::Outputs::UDP.new({"host" => host, "port" => port}) }
-
     let(:properties) { { "message" => "This is a message!"} }
     let(:event)      { LogStash::Event.new(properties) }
 
@@ -24,7 +23,40 @@ describe LogStash::Outputs::UDP do
 
     it "should receive the generated event" do
       expect(subject.instance_variable_get("@socket")).to receive(:send).with(kind_of(String), 0, host, port)
+      expect(subject.instance_variable_get("@logger")).not_to receive(:error)
       subject.receive(event)
     end
+  end
+
+  describe "large message" do
+    let(:properties) { { "message" => "0" * 65_536 } }
+    let(:event)      { LogStash::Event.new(properties) }
+
+    before(:each) do
+      subject.register
+    end
+
+    it "should handle the error and log when an error is received" do
+      expect(subject.instance_variable_get("@logger")).to receive(:error)
+      subject.receive(event)
+    end
+
+    it "should log a truncated payload with debug logging when an error is received and the message is too long" do
+      expect(subject.instance_variable_get("@logger")).to receive(:debug?).and_return(true)
+      expect(subject.instance_variable_get("@logger")).to receive(:error) do |_, hash|
+        expect(hash).to include(:event_payload)
+        expect(hash[:event_payload]).to include("TRUNCATED")
+      end
+      subject.receive(event)
+    end
+
+    it "should not log a payload with debug logging when an error is received" do
+      expect(subject.instance_variable_get("@logger")).to receive(:debug?).and_return(false)
+      expect(subject.instance_variable_get("@logger")).to receive(:error) do |_, hash|
+        expect(hash).not_to include(:event_payload)
+      end
+      subject.receive(event)
+    end
+
   end
 end
